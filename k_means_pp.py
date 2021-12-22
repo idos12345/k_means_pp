4db983bbf4587aba82dd915e0a993002ce6771b1
Ido Shapira <idoshapira2@mail.tau.ac.il>
2021-12-22 09:55:41 +0200

    1

--- k_means_pp.py
+++ k_means_pp.py
@@ -7,7 +7,7 @@ import pandas as pd
 def calc_min_dist(rows, np_data, mus, curr_num_of_mus):
     d_arr = np.zeros(rows)
     for l in range(rows):
-        dl = min([np.linalg.norm(find_row_by_index(l, np_data)[1:] - mus[i][1:]) for i in range(curr_num_of_mus)])
+        dl = min([np.sum((find_row_by_index(l, np_data)[1:] - mus[i][1:]) ** 2) for i in range(curr_num_of_mus)])
         d_arr[l] = dl
 
     return d_arr
@@ -33,24 +33,20 @@ def k_means_pp(k, max_iter, eps, input_1_filename, input_2_filename):
     data_2 = pd.read_csv(input_2_filename, sep=",", header=None)
 
     data = pd.merge(data_1, data_2, on=0)
-
     np_data = data.to_numpy()
+
     rows = len(np_data)
     cols = len(np_data[0])
 
     mus = np.zeros([k, cols])
-
     chosen_row = np.random.choice(range(rows))
-    print(chosen_row)
     mus[0] = find_row_by_index(chosen_row, np_data)
 
+
     for i in range(1, k):
-        print(mus)
         dist_arr = calc_min_dist(rows, np_data, mus, i)
         probs_arr = calc_probs(dist_arr)
-        print(sum(probs_arr))
         chosen_row = np.random.choice(range(rows), p=probs_arr)
-        print(chosen_row)
         mus[i] = find_row_by_index(chosen_row, np_data)
 
     print(mus)
@@ -97,10 +93,10 @@ def submit_args():
 
 
 def main():
-    file1 = "input_1_db_1.txt"
-    file2 = "input_1_db_2.txt"
+    file1 = "input_2_db_1.txt"
+    file2 = "input_2_db_2.txt"
     # args = submit_args()
-    args = [3, 333, 0, file1, file2]
+    args = [7, 300, 0, file1, file2]
     if args == 0:
         return 0
     k, max_iter, eps, input_1, input_2 = args
